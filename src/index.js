f38b897ea822cfe6404ef554561973fdc91d3112
oded-saban <odeds@activefence.com>
2022-07-08 17:17:11 +0300

    add repo detection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require("./router");
+const detectionRouter = require("./router");
 const port = 8000;
 const main = async () => {
     const app = express();
     app.use(bodyParser.urlencoded({ extended: true }));
     // parse application/json
     app.use(bodyParser.json());
-    app.use('', router);
+    app.use('/detection', detectionRouter);
     app.listen(port, () => console.log(`Server is listening to port ${port}`));
 
 }
--- src/router.js
+++ src/router.js
@@ -11,14 +11,23 @@ router.post('/detect', async (req,res) => {
             const pushTimestamp = bodyData["repository"].pushed_at;
             const pushDate = new Date(pushTimestamp * 1000);
             const pushHour = pushDate.getHours();
-            console.log(pushHour);
             if (pushHour >= 14 && pushHour <= 16) {
                 console.warn(`We detected suspicious behavior, someone pushed code between 14:00 - 16:00`);
             }
         }
+        if (bodyData["action"] === "deleted" && bodyData["repository"]) {
+            const {created_at, updated_at} = bodyData["repository"];
+            const createTimestamp = Date.parse(created_at);
+            const deleteTimestamp = Date.parse(updated_at);
+            const minutesBetweenTimestamps = (deleteTimestamp - createTimestamp) / 60_000;
+            if (minutesBetweenTimestamps < 10) {
+                console.warn(`We detected suspicious behavior, someone created a repository and deleted it in less than 10 minutes`);
+            }
+        }
         res.status(200);
     }catch (e) {
-
+        console.error(`Operation failed with error =>  ${e}`);
+        return res.status(500);
     }})
 
 module.exports = router;
