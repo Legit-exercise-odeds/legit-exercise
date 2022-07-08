0bc6944f119bde30124bd62f14a1ee0bd5fe2eaf
oded-saban <odeds@activefence.com>
2022-07-08 12:55:17 +0300

    add push detection

--- src/router.js
+++ src/router.js
@@ -10,7 +10,8 @@ router.post('/detect', async (req,res) => {
         if (bodyData["repository"]) {
             const pushTimestamp = bodyData["repository"].pushed_at;
             const pushDate = new Date(pushTimestamp * 1000);
-            const pushHour = 14;
+            const pushHour = pushDate.getHours();
+            console.log(pushHour)
             if (pushHour >= 14 && pushHour <= 16) {
                 console.warn(`We detected suspicious behavior, someone pushed code between 14:00 - 16:00`);
             }
