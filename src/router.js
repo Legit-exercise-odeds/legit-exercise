735fe0bd5679fe377e9dab9d0d349b7cf1603ba4
oded-saban <odeds@activefence.com>
2022-07-08 12:40:57 +0300

    add push detection

--- src/router.js
+++ src/router.js
@@ -8,8 +8,10 @@ router.post('/detect', async (req,res) => {
             console.warn(`We detected suspicious behavior, a team with the name: ${bodyData["team"].name} was created`);
         }
         if (bodyData["repository"]) {
-            const pushedTimestamp = bodyData["repository"].pushed_at;
-            console.log(pushedTimestamp)
+            const pushTimestamp = bodyData["repository"].pushed_at;
+            const pushDate = new Date(pushTimestamp * 1000);
+            const pushHour = pushDate.getHours();
+            console.log(pushHour)
         }
         res.status(200);
     }catch (e) {
