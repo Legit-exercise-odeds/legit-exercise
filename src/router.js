fa21dd60043668740416722d491e653e3cf1cab5
oded-saban <odeds@activefence.com>
2022-07-08 12:10:48 +0300

    add team detection

--- src/router.js
+++ src/router.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 router.post('/detect', async (req,res) => {
     try {
-        console.log('oded')
+        const bodyData = req.body;
+        if (bodyData["team"] && bodyData["action"] === "created" && bodyData["team"].name.startsWith("hacker")) {
+            console.warn(`We detected suspicious behavior, a team with the name: ${bodyData["team"].name} was created`);
+        }
         res.status(200);
     }catch (e) {
 
