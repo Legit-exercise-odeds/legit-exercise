d78025fa48be2fd2ce0197f6b7ff39aa61e08667
oded-saban <odeds@activefence.com>
2022-07-09 22:58:13 +0300

    separate routs

--- src/router.js
+++ src/router.js
@@ -1,36 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/detect', async (req,res) => {
-    try {
-        const bodyData = req.body;
-        if (bodyData["team"] && bodyData["action"] === "created" && bodyData["team"].name.startsWith("hacker")) {
-            console.warn(`We detected suspicious behavior, a team with the name: ${bodyData["team"].name} was created`);
-        }
-        if (bodyData["action"] === "deleted" && bodyData["repository"]) {
-            const {created_at, updated_at} = bodyData["repository"];
-            const createTimestamp = Date.parse(created_at);
-            const deleteTimestamp = Date.parse(updated_at);
-            const minutesBetweenTimestamps = (deleteTimestamp - createTimestamp) / 60_000;
-            if (minutesBetweenTimestamps < 10) {
-                console.warn(`We detected suspicious behavior, someone created a repository and deleted it in less than 10 minutes`);
-            }
-        }
-        res.status(200);
-    }catch (e) {
-        console.error(`Operation failed with error =>  ${e}`);
-        return res.status(500);
-    }});
-
 router.post('/pushes', async (req, res) => {
     try {
-        const { repository, ref } = req.body;
+        const { repository, ref, sender } = req.body;
         if (ref) {
             const pushTimestamp = repository.pushed_at;
             const pushDate = new Date(pushTimestamp * 1000);
-            const pushHour = 14;
+            const pushHour = pushDate.getHours();
             if (pushHour >= 14 && pushHour <= 16) {
-                console.warn(`We detected suspicious behavior, someone pushed code between 14:00 - 16:00`);
+                console.warn(`We detected suspicious behavior, ${sender.login} pushed code between 14:00 - 16:00`);
             } else {
                 console.log("hello")
             }
@@ -41,6 +20,38 @@ router.post('/pushes', async (req, res) => {
         console.error(`Operation failed with error =>  ${e}`);
         return res.status(500);
     }
-})
+});
+
+router.post('/teams', async (req, res) => {
+    try {
+        const {team, action, sender} = req.body;
+        if (team && action === "created" && team.name.startsWith("hacker")) {
+            console.warn(`We detected suspicious behavior, a team with the name: ${team.name} was created by ${sender.login}`);
+        }
+        return res.status(200);
+    }catch (e) {
+        console.error(`Operation failed with error =>  ${e}`);
+        return res.status(500);
+    }
+});
+
+router.post('/repositories', async (req, res) => {
+    try {
+        const {repository, action, sender} = req.body;
+        if (action === "deleted" && repository) {
+            const {created_at, updated_at, name} = repository;
+            const createTimestamp = Date.parse(created_at);
+            const deleteTimestamp = Date.parse(updated_at);
+            const minutesBetweenTimestamps = (deleteTimestamp - createTimestamp) / 60_000;
+            if (minutesBetweenTimestamps < 10) {
+                console.warn(`We detected suspicious behavior, ${sender.login} created a repository with name : ${name} and deleted it in less than 10 minutes`);
+            }
+        }
+        return res.status(200);
+    }catch (e) {
+        console.error(`Operation failed with error =>  ${e}`);
+        return res.status(500);
+    }
+});
 
 module.exports = router;
